101c0392b5f4df5c8ddc5714a7459555df86d981
elieiradukunda <iradukundaelie71@gmail.com>
2025-11-03 16:48:53 +0200

    connecting to neon deb

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,14 @@
-import pg from "pg";
+import { Pool } from 'pg';
 import dotenv from "dotenv";
 dotenv.config();
 
-const { Pool } = pg;
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in .env");
+}
+
+const isProduction = process.env.DATABASE_URL.includes("neon");
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
